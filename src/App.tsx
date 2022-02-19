1943148806b7da5aae170357cec486cb09d0aed5
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-18 20:36:49 -0500

    Create logo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Logo from "./Components/Logo/Logo"
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props: any) {
+    super(props)
+  }
+  render() :  JSX.Element {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Logo />
     </div>
   );
+}
 }
 
 export default App;
---
+++ src/Components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+import "./Logo.css"
+import React from "react"
+
+const Logo = () => {
+    return (
+        <div>
+            <p className="top-line">Art<span className="top-line-2">Block</span></p>
+            <p className="bottom-line">Where Artists Come For Random Inspiration</p>
+        </div>
+    )
+}
+
+export default Logo;
