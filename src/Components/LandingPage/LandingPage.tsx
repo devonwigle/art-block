cdec6f95c9dfe7c7705fee08f1c87466ab6fe6ad
Emma Chory <emmachory@gmail.com>
2022-02-20 10:25:17 -0700

    Create landingpage files and import logo

---
+++ src/Components/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+
+const LandingPage = () => {
+  return(
+    <div>
+    <Logo />
+    <p></p>
+    <button>Button</button>
+    </div>
+  )
+}
+
+export default LandingPage
