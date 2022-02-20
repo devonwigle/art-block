7bb5a5fa822cbe2c7b5dd1775d973bd0f21c311b
Emma Chory <emmachory@gmail.com>
2022-02-20 10:36:00 -0700

    Style landing button and paragraphs

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Logo from '../Logo/Logo'
+import './LandingPage.css'
 
 const LandingPage = () => {
   return(
     <div>
-    <Logo />
-    <p></p>
-    <button>Button</button>
+      <Logo />
+      <div className="landing-paragraphs">
+        <p>Are you feeling the unending agony that is being blocked from pursuing your artistic passions?</p>
+        <p>We have found the solution for you.  Click below to have inspiration brought forth from the internet.</p>
+        <p>We will provide you a random color, image, and word(s) of inspiration.</p>
+      </div>
+      <button className="landing-button">Be Inspired</button>
     </div>
   )
 }
