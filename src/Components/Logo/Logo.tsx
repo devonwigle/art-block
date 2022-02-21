bc83a7f0462b045a0bfd8334736ea4f9b577ffd1
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-21 12:38:30 -0500

    Complete landing page cypress testing

--- src/Components/Logo/Logo.tsx
+++ src/Components/Logo/Logo.tsx
@@ -1,13 +1,15 @@
-import "./Logo.css"
-import React from "react"
+import "./Logo.css";
+import React from "react";
 
 const Logo = () => {
-    return (
-        <div>
-            <p className="top-line">Art<span className="top-line-2">BLOCK</span></p>
-            <p className="bottom-line">Where Artists Come For Random Inspiration</p>
-        </div>
-    )
-}
+  return (
+    <div className="big-logo">
+      <p className="top-line">
+        Art<span className="top-line-2">BLOCK</span>
+      </p>
+      <p className="bottom-line">Where Artists Come For Random Inspiration</p>
+    </div>
+  );
+};
 
 export default Logo;
