2df38f1112d17397d661535d2ccb18da48e0fc55
Devon Wigle <devonwigle@gmail.com>
2022-02-20 23:02:45 -0600

    Change colors, size and position of cube

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -17,10 +17,12 @@ const LandingPage = () => {
             <div className="bottom"></div>
             <div className="shadow"></div>
           </div>
-        </div>  
-        <p>Are you feeling the unending agony that is being blocked from pursuing your artistic passions?</p>
-        <p>We have found the solution for you.  Click below to have inspiration brought forth from the internet.</p>
-        <p>We will provide you a random color, image, and word(s) of inspiration.</p>
+        </div>
+        <div className="para-box">  
+          <p>Are you feeling the unending agony that is being blocked from pursuing your artistic passions?</p>
+          <p>We have found the solution for you.  Click below to have inspiration brought forth from the internet.</p>
+          <p>We will provide you a random color, image, and word(s) of inspiration.</p>
+        </div>
       </div>
       <button className="landing-button">Be Inspired</button>
       
