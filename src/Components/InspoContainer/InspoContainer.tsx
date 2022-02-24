e9db131aa405f0f0534bc355b7ef0007cbc9180b
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-24 14:12:14 -0500

    Add ? button for instructions modal

--- src/Components/InspoContainer/InspoContainer.tsx
+++ src/Components/InspoContainer/InspoContainer.tsx
@@ -5,6 +5,8 @@ import "./InspoContainer.css";
 import { PicsumImage, Word } from "../../apiCalls/apiCalls";
 import ColorContainer from "../ColorContainer/ColorContainer";
 import { Link } from "react-router-dom";
+import mark1 from "./mark1.png";
+import mark2 from "./mark2.png";
 
 type InspoContainerProps = {
   picture: PicsumImage | string;
@@ -20,6 +22,9 @@ type InspoContainerProps = {
 const InspoContainer = (props: InspoContainerProps) => {
   return (
     <div className="Inspo-container">
+      <button className="question-button" onClick={() => console.log("click")}>
+        <img className="mark-img" src={mark1} />
+      </button>
       <div className="contents">
         <PictureContainer
           picture={props.picture}
