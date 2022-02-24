e7df33716c6bc48627ff9c9d592f2468669f5b1f
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-24 14:54:02 -0500

    Style modal

--- src/Components/InspoContainer/InspoContainer.tsx
+++ src/Components/InspoContainer/InspoContainer.tsx
@@ -26,8 +26,37 @@ const InspoContainer = (props: InspoContainerProps) => {
       <button className="question-button" onClick={() => setModalOpen(true)}>
         <img className="mark-img" src={mark1} />
       </button>
-      <Modal isOpen={modalOpen} onRequestClose={() => setModalOpen(false)}>
-        <p>Instructions</p>
+      <Modal
+        className="Modal"
+        isOpen={modalOpen}
+        onRequestClose={() => setModalOpen(false)}
+      >
+        <h1>Instructions</h1>
+        <p>
+          Welcome to ArtBlock! On this page, you can find inspiration in three
+          forms, an image, a descriptive word, or a color (hexcode provided).
+        </p>
+        <p>
+          We'll create the first trio of inspiration for you. If you don't like
+          anything, feel free to click the Reinspire button to get a new image,
+          word, and color.
+        </p>
+        <p>
+          Perhaps you like an image, but not the word and color. Feel free to
+          select the checkbox under the image to "lock it" in place. When you
+          click Respire, it will hold onto that image until you uncheck the box.
+          You can lock any combination of the three.
+        </p>
+        <p>
+          If you're happy with your trio of inspiration, go ahead and click the
+          Save Inspiration button! You can save as many trios as you'd like.
+        </p>
+        <p>
+          Finally, to see all your saved Inspirations, click the See my
+          Inspiration button to head to the favorites page! We hope to bring you
+          some inspiration for your next work of art!
+        </p>
+        <p>Click the ESC key or outside of this popup to leave this page.</p>
       </Modal>
       <div className="contents">
         <PictureContainer
