602b8d44c6d1e20dbc945d23c94cd5e59461226c
Rana Jurjus <rjurjus11@mail.bw.edu>
2022-02-24 14:57:03 -0500

    Add classname to p in modal for better styling

--- src/Components/InspoContainer/InspoContainer.tsx
+++ src/Components/InspoContainer/InspoContainer.tsx
@@ -32,31 +32,33 @@ const InspoContainer = (props: InspoContainerProps) => {
         onRequestClose={() => setModalOpen(false)}
       >
         <h1>Instructions</h1>
-        <p>
+        <p className="modal-text">
           Welcome to ArtBlock! On this page, you can find inspiration in three
           forms, an image, a descriptive word, or a color (hexcode provided).
         </p>
-        <p>
+        <p className="modal-text">
           We'll create the first trio of inspiration for you. If you don't like
           anything, feel free to click the Reinspire button to get a new image,
           word, and color.
         </p>
-        <p>
+        <p className="modal-text">
           Perhaps you like an image, but not the word and color. Feel free to
           select the checkbox under the image to "lock it" in place. When you
           click Respire, it will hold onto that image until you uncheck the box.
           You can lock any combination of the three.
         </p>
-        <p>
+        <p className="modal-text">
           If you're happy with your trio of inspiration, go ahead and click the
           Save Inspiration button! You can save as many trios as you'd like.
         </p>
-        <p>
+        <p className="modal-text">
           Finally, to see all your saved Inspirations, click the See my
           Inspiration button to head to the favorites page! We hope to bring you
           some inspiration for your next work of art!
         </p>
-        <p>Click the ESC key or outside of this popup to leave this page.</p>
+        <p className="modal-text">
+          Click the ESC key or outside of this popup to leave this page.
+        </p>
       </Modal>
       <div className="contents">
         <PictureContainer
