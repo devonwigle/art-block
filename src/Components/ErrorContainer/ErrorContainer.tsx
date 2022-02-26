9a1ba585e4ad393dae52859fc5c6ed138bdac9ff
Devon Wigle <devonwigle@gmail.com>
2022-02-25 18:09:15 -0600

    Expand error message

--- src/Components/ErrorContainer/ErrorContainer.tsx
+++ src/Components/ErrorContainer/ErrorContainer.tsx
@@ -4,7 +4,7 @@ import './ErrorContainer.css'
 const ErrorContainer = () => {
   return (
     <div className='message-box'>
-      <p className='message-text'>Oops! Something went wrong, please reinspire</p>
+      <p className='message-text'>Oops! Something went wrong, please reinspire! You will not be able to save this inspiration</p>
     </div>
   )
 }
